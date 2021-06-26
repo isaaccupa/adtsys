94525bfabae7d96a85523bf26377f6913b96505c
isaaccupa <dedao@dedao_pc.localdomain>
2021-06-26 18:52:34 -0300

    add index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+# Fill our vars and run on cli
+# $ php -f db-connect-test.php
+
+$dbname = 'adtsys';
+$dbuser = 'adtsys_admin';
+$dbpass = 'adtsys123';
+$dbhost = '10.0.2.5';
+
+error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
+$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
+
+$test_query = "SHOW TABLES FROM $dbname";
+$result = mysqli_query($link, $test_query);
+
+$tblCnt = 0;
+while($tbl = mysqli_fetch_array($result)) {
+  $tblCnt++;
+  #echo $tbl[0]."<br />\n";
+}
+
+if (!$tblCnt) {
+  echo "Connected successfully.\NThere are no tables<br\n";
+} else {
+  echo "Connected successfully.\nhere are $tblCnt tables<br\n";
+}
